bc176c26fbda8f01adafb922d3b0d9c13897bcc4
Exaucé <47887636+exauce21@users.noreply.github.com>
2021-06-14 19:48:57 +0000

    Gestion de chargement dans une liste et  de dialogue

--- app/src/main/java/com/example/myschool/FormationFragment.java
+++ app/src/main/java/com/example/myschool/FormationFragment.java
@@ -1,5 +1,7 @@
 package com.example.myschool;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class FormationFragment extends Fragment {
@@ -23,6 +27,28 @@ public class FormationFragment extends Fragment {
         listFormation = view.findViewWithTag(R.id.listFormation);
         tabFormation = getResources().getStringArray(R.array.tab_formation);
         tabDetails = getResources().getStringArray(R.array.tab_details);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, tabFormation);
+        listFormation.setAdapter(adapter);//Chargement des données sur la liste
+
+        listFormation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                formtation = tabFormation[position];
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                dialog.setIcon(R.mipmap.ic_launcher);
+                dialog.setTitle(formtation);
+                dialog.setMessage(details);
+                dialog.setNegativeButton(getString(R.string.back), null);
+                dialog.setPositiveButton(getString(R.string.sign_up), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();//Affiche la boite de dialogue
+            }
+        });
 
         return view;
     }
